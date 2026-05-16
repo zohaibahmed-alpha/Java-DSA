7194f2237a51647bc1f68dc118575013affa9410
zohaibahmed-alpha <Zohaibahmed4322@gmail.com>
2026-05-16 15:39:18 +0530

    add Lec5

---
+++ Lec5.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Lec5 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = 5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+             int spaces = 2 * (n-i);
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+             int spaces = 2 * (n-i);
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
