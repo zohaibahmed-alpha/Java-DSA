f2d3d8476822697a89e9b7c174f17139c4477914
zohaibahmed-alpha <Zohaibahmed4322@gmail.com>
2026-05-16 16:27:00 +0530

    add Lec5

--- Lec5.java
+++ Lec5.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Lec5 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int n = 4;
+        int n = 5;
         // Number pyramid
         // for(int i=1; i<=n; i++){
         //     for(int j=1; j<=n-i; j++){
@@ -29,24 +29,52 @@ public class Lec5 {
         // }
 
         // Diamond pattern
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n-i; j++){
-                System.out.print(" ");
-            }
-            for(int j=1; j<=2*i-1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for(int i=n; i>=1; i--){
-            for(int j=1; j<=n-i; j++){
-                System.out.print(" ");
-            }
-            for(int j=1; j<=2*i-1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-    }
+    //     for(int i=1; i<=n; i++){
+    //         for(int j=1; j<=n-i; j++){
+    //             System.out.print(" ");
+    //         }
+    //         for(int j=1; j<=2*i-1; j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    //     for(int i=n; i>=1; i--){
+    //         for(int j=1; j<=n-i; j++){
+    //             System.out.print(" ");
+    //         }
+    //         for(int j=1; j<=2*i-1; j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    // }
     
+    // Butterfly pattern
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        int spaces = 2 * (n-i);
+        for(int j=1; j<=spaces; j++){
+            System.out.print(" ");
+        }
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        int spaces = 2 * (n-i);
+        for(int j=1; j<=spaces; j++){
+            System.out.print(" ");
+        }
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 }
 }
