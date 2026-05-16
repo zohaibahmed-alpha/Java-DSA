fbc21410a7d9b7d5d5ad9c00ca6962ad96b2d0b7
zohaibahmed-alpha <Zohaibahmed4322@gmail.com>
2026-05-16 16:18:40 +0530

    add Lec5

--- Lec5.java
+++ Lec5.java
@@ -2,7 +2,8 @@ import java.util.*;
 public class Lec5 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int n = 5;
+        int n = 4;
+        // Number pyramid
         // for(int i=1; i<=n; i++){
         //     for(int j=1; j<=n-i; j++){
         //         System.out.print(" ");
@@ -12,19 +13,40 @@ public class Lec5 {
         //     }
         //     System.out.println();
         // }
+
+        // Palindromic pyramid
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=i; j>=1; j--){
+        //         System.out.print(j);
+        //     }
+        //     for(int j=2; j<=i; j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+        // Diamond pattern
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n-i; j++){
                 System.out.print(" ");
             }
-            for(int j=i; j>=1; j--){
-                System.out.print(j);
-            }
-            for(int j=2; j<=i; j++){
-                System.out.print(j);
+            for(int j=1; j<=2*i-1; j++){
+                System.out.print("*");
             }
             System.out.println();
         }
-
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
     }
     
+}
 }
