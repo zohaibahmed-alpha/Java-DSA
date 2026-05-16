ea4672c8114607008ad79d1881edd33742d594de
zohaibahmed-alpha <Zohaibahmed4322@gmail.com>
2026-05-16 16:07:49 +0530

    add Lec5

--- Lec5.java
+++ Lec5.java
@@ -3,12 +3,24 @@ public class Lec5 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = 5;
+        // for(int i=1; i<=n; i++){
+        //     for(int j=1; j<=n-i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1; j<=i; j++){
+        //         System.out.print( " " + i + " ");
+        //     }
+        //     System.out.println();
+        // }
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n-i; j++){
                 System.out.print(" ");
             }
-            for(int j=1; j<=i; j++){
-                System.out.print( " " + i + " ");
+            for(int j=i; j>=1; j--){
+                System.out.print(j);
+            }
+            for(int j=2; j<=i; j++){
+                System.out.print(j);
             }
             System.out.println();
         }
