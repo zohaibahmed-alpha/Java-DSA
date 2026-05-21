212be718de9a975d4240eaebe60a4f5fb1e5e3e6
zohaibahmed-alpha <Zohaibahmed4322@gmail.com>
2026-05-22 01:28:54 +0530

    Add changes

--- Lec6func.java
+++ Lec6func.java
@@ -1,14 +1,24 @@
 import java.util.*;
 public class Lec6func {
-    public static int printMul(int a, int b){
-        return a*b;
+    // public static int printMul(int a, int b){
+    //     return a*b;
+    // }
+    public static void printFact(int n){
+        int factorial = 1;
+        if(n < 0) {
+            System.out.println("invalid num");
+            return;
+        }
+        for(int i=n; i>=1; i--){
+            factorial = factorial * i;
+        }
+         System.out.println(factorial);
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int n = sc.nextInt();
+        printFact(n);
         
-        System.out.println(printMul(a , b));
 
     }
     
---
+++ Lec6prac.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Lec6prac {
+    public static void printSum(int n){
+        int sum = 0;
+      for(int i=1; i<=n; i++){
+        System.out.println(i);
+        if(i % 2 != 0){
+           sum = sum + i;
+        }
+      }
+      System.out.println(sum);
+    }
+    
+    
+        public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printSum(n);
+        
+    }
+    
+}
