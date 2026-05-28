807881bb0c2e6589a21077417ed45e2da68210c3
zohaibahmed-alpha <Zohaibahmed4322@gmail.com>
2026-05-29 00:38:10 +0530

    Add changes

--- Lec9.java
+++ Lec9.java
@@ -3,14 +3,11 @@ public class Lec9 {
     public static void main (String args[]){
         int nums[] = {1,2,3,4,5,6,10};
         Scanner sc = new Scanner(System.in);
-        int min = nums[0];
+        int sum = 0;
         for(int i=0; i<nums.length;i++){
-         if(nums[i] < min) {
-            min = nums[i];
-         }
-         
+         sum = sum + nums[i];
         }
-        System.out.println("min nums is:" + min);
+        System.out.println("sum is:" + sum);
     }
     
 }
