3c3fea4b9db5458da05a668e48bbb15570021348
zohaibahmed-alpha <Zohaibahmed4322@gmail.com>
2026-05-29 16:28:19 +0530

    Reverse an array

---
+++ leetcode/Arrayreverse.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class Arrayreverse {
+    public static void main(String args[]){
+        int nums[] = {1,2,3,4,5,6,7,8,9,10};
+        for(int i=nums.length-1; i>=0; i--){
+            System.out.print(nums[i] + " ");
+        }
+        
+    }
+    
+}
