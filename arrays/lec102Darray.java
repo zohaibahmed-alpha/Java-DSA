6c8abd8a6eaffe67a922f8a5202d5ef1aa6ed3a8
zohaibahmed-alpha <Zohaibahmed4322@gmail.com>
2026-05-29 20:27:22 +0530

    Add finding location in a matrix

--- arrays/lec102Darray.java
+++ arrays/lec102Darray.java
@@ -4,21 +4,25 @@ public class lec102Darray {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
-        int columns = sc.nextInt();
-        int matrix[][] = new int[rows][columns];
+        int cols = sc.nextInt();
+        int matrix[][] = new int[rows][cols];
         
         for(int i=0; i<rows; i++){
-          for(int j=0; j<columns; j++){
+          for(int j=0; j<cols; j++){
             matrix [i][j] = sc.nextInt();
           }
         }
+        int x = sc.nextInt();
         for(int i=0; i<rows; i++){
-            for(int j=0; j<columns; j++){
-                System.out.print(matrix[i][j] + " ");
+            for(int j=0; j<cols; j++){
+                if(matrix[i][j] == x){
+                    System.out.println("x found at location :( "+ i +", "+ j +")");
+                }
             }
-            System.out.println();
         }
+        }
+
     }
 
     
-}
+
